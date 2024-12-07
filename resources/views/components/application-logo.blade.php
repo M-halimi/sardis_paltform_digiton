a7104e560c86dc35224c8c741b0cc6cc45052f57
M-halimi <halimimihammed656@gmail.com>
2024-12-07 18:33:44 +0100

    mh:fix footer

--- resources/views/components/application-logo.blade.php
+++ resources/views/components/application-logo.blade.php
@@ -1,4 +1,4 @@
 <img {{ $attributes->merge(['class' => ""]) }} src="{{ asset('assets/images/logo-sardis.png') }}"
-     class="max-h-32"
+     class="max-h-12"
      alt="{{ config('app.name') }}'s logo"
      width="100" height="100">
--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,157 +1,129 @@
-<footer class="max-w-7xl mx-auto grid grid-cols-2 lg:grid-cols-5 px-4">
-    <div class="py-4 col-span-2 lg:col-span-1">
-        <a href="/" class="animate-spin">
-            <x-application-logo class="md:w-[150px] w-[100px]"/>
+<footer class="max-w-7xl mx-auto">
+    <div class="flex py-4">
+        <a href="/" class="#">
+            <x-application-logo class="md:w-[150px] w-[100px] mx-auto"/>
         </a>
     </div>
-    <div class="flex justify-center items-center max-h-max">
-        <div class="flex flex-col justify-center col-span-1 space-y-4">
-            <h2 class="text-2xl font-bold text-persian-plum-950">
-                Pourquoi Sardis ?
-            </h2>
-            <p class="text-lg font-normal tracking-wide">
-                En choisissant Sarids, vous bénéficierez d'un suppor
-                t de qualité et de notre expertise pour vous accompagner tout au long de votre projet sanitaire.
-                Nous sommes là pour vous aider à trouver la meilleure solution sanitaire.
-                Sardis, Le partenaire exclusif de Ramon soler au maroc !
-            </p>
+    <div class="grid grid-cols-2 lg:grid-cols-4 gap-4 px-4">
+        <div class="flex justify-center items-center max-h-max">
+            <div class="flex flex-col space-y-4">
+                <h2 class="text-2xl font-bold text-persian-plum-950">
+                    Pourquoi Sardis ?
+                </h2>
+                <p class="text-lg font-normal tracking-wide">
+                    En choisissant Sardis, vous bénéficierez d'un support de qualité et de notre expertise pour vous
+                    accompagner tout au long de votre projet sanitaire.
+                    Nous sommes là pour vous aider à trouver la meilleure solution sanitaire.
+                    Sardis, Le partenaire exclusif de Ramon Soler au Maroc !
+                </p>
+            </div>
+        </div>
+        <div class="flex justify-center items-center max-h-max ">
+            <div class="flex flex-col space-y-4">
+                <h2 class="text-left text-2xl font-bold text-persian-plum-950">
+                    Liens utiles
+                </h2>
+                <ul class="space-y-3.5">
+                    <li><a href="#"
+                           class="hover:underline hover:decoration-cobalt-700 hover:underline-offset-2">Contact</a></li>
+                    <li><a href="#" class="hover:underline hover:decoration-cobalt-700 hover:underline-offset-2">À
+                            propos</a></li>
+                    <li><a href="#" class="hover:underline hover:decoration-cobalt-700 hover:underline-offset-2">Politique
+                            de confidentialité</a></li>
+                    <li><a href="#" class="hover:underline hover:decoration-cobalt-700 hover:underline-offset-2">Politique
+                            de Garantie</a></li>
+                    <li><a href="#"
+                           class="hover:underline hover:decoration-cobalt-700 hover:underline-offset-2">SiteMap</a></li>
+                </ul>
+            </div>
+        </div>
+        <div class="flex justify-center items-center max-h-max ">
+            <div class="flex flex-col space-y-4">
+                <h2 class="text-left text-2xl font-bold text-persian-plum-950">
+                    Produits
+                </h2>
+                <ul class="space-y-3.5">
+                    <li>Robinetterie</li>
+                    <li>Salle de bain</li>
+                    <li>Clapet et regard</li>
+                    <li>Siphon et caniveau</li>
+                </ul>
+            </div>
+        </div>
+        <div class="flex justify-center items-center max-h-max ">
+            <div class="flex flex-col space-y-4">
+                <h2 class="text-2xl font-bold text-persian-plum-950">
+                    Contact Info
+                </h2>
+                <p class="text-lg font-normal tracking-wide">
+                    En choisissant Sardis, vous bénéficierez d'un support de qualité et de notre expertise pour vous
+                    accompagner tout au long de votre projet sanitaire.
+                    Nous sommes là pour vous aider à trouver la meilleure solution sanitaire.
+                    Sardis, Le partenaire exclusif de Ramon Soler au Maroc !
+                </p>
+            </div>
         </div>
     </div>
-
-    <div class="flex justify-center items-center max-h-max">
-        <div class="flex flex-col justify-center col-span-1 space-y-4">
-            <h2 class="text-left text-2xl font-bold text-persian-plum-950">
-                Liens utiles
-            </h2>
-            <ul class="block space-y-3.5">
-                <a href="#" class="block hover:underline hover:decoration-cobalt-700 hover:underline-offset-2">
-                    <li class="#">
-                        Contact
-                    </li>
-                </a>
-
-                <a href="#" class="block hover:underline hover:decoration-cobalt-700 hover:underline-offset-2">
-                    <li class="#">
-                        A propos
-                    </li>
-                </a>
-
-                <a href="#" class="block hover:underline hover:decoration-cobalt-700 hover:underline-offset-2">
-                    <li class="#">
-                        Politique de confidentialité
-                    </li>
-                </a>
-
-                <a href="#" class="block hover:underline hover:decoration-cobalt-700 hover:underline-offset-2">
-                    <li class="#">
-                        Politique de Garantie
-                    </li>
-                </a>
-
-                <a href="#" class="block hover:underline hover:decoration-cobalt-700 hover:underline-offset-2">
-                    <li>
-                        SiteMap
-                    </li>
-                </a>
-            </ul>
-        </div>
-    </div>
-
-    <div class="flex justify-center items-center max-h-max">
-        <div class="flex flex-col justify-center col-span-1 space-y-4">
-            <h2 class="text-left text-2xl font-bold text-persian-plum-950">
-                Liens utiles
-            </h2>
-            <ul class="block space-y-4">
-                <li>
-                    Robinetterie
-                </li>
-                <li>
-                    Salle de bain
-                </li>
-                <li>
-                    Clapet et regard
-                </li>
-                <li>
-                    Siphon et caniveau
-                </li>
-            </ul>
-        </div>
-    </div>
-
-    <div class="flex justify-center items-center max-h-max">
-        <div class="flex flex-col justify-center col-span-1 space-y-4">
-            <h2 class="text-2xl font-bold text-persian-plum-950">
-                Contact Info
-            </h2>
-            <p class="text-lg font-normal tracking-wide">
-                En choisissant Sarids, vous bénéficierez d'un suppor
-                t de qualité et de notre expertise pour vous accompagner tout au long de votre projet sanitaire.
-                Nous sommes là pour vous aider à trouver la meilleure solution sanitaire.
-                Sardis, Le partenaire exclusif de Ramon soler au maroc !
-            </p>
-        </div>
-    </div>
-    <hr class="border drop-shadow-3xl  bg-cobalt-900 w-full h-1.5 col-span-6 "/>
-    <div class="flex justify-end items-end py-12 ">
-        <div>
-            <span class="block py-4 w-32 text-end">
-                <svg xmlns="http://www.w3.org/2000/svg"
-                     width="30"
-                     height="30"
-                     class="ionicon" viewBox="0 0 512 512">
-
-                      <path d="M480 257.35c0-123.7-100.3-224-224-224s-224 100.3-224 224c0 111.8 81.9 204.47 189
-                      221.29V322.12h-56.89v-64.77H221V208c0-56.13 33.45-87.16 84.61-87.16 24.51 0 50.15 4.38 50.15
-                       4.38v55.13H327.5c-27.81 0-36.51 17.26-36.51 35v42h62.12l-9.92 64.77H291v156.54c107.1-16.81 189-109.48 189-221.31"
-                            fill-rule="evenodd"
-                      />
-                </svg>
+    <hr class="border bg-cobalt-900 w-full h-0.5 mt-4"/>
+    <div class="flex flex-col md:flex-row justify-between items-center space-x-4">
+        <ul class="flex justify-center items-center h-32 space-x-4 order-2">
+            <li class="relative group">
+                <div class="w-12 h-12 bg-white text-lg text-gray-900 flex justify-center items-center rounded-full shadow-lg transition-transform transform group-hover:scale-110 cursor-pointer">
+        <span class="absolute top-[-40px] left-1/2 transform -translate-x-1/2 bg-cobalt-900 text-sm text-white px-2 py-1 rounded-md shadow-md opacity-0 group-hover:opacity-100 group-hover:translate-y-[-10px] transition duration-300">
+          Facebook
         </span>
-
-        </div>
-
-
-        <span class="block py-4 w-32">
-                <svg xmlns="http://www.w3.org/2000/svg"
-                     width="30"
-                     height="30"
-                     class="ionicon" viewBox="0 0 512 512">
-
-                      <path d="M480 257.35c0-123.7-100.3-224-224-224s-224 100.3-224 224c0 111.8 81.9 204.47 189
-                      221.29V322.12h-56.89v-64.77H221V208c0-56.13 33.45-87.16 84.61-87.16 24.51 0 50.15 4.38 50.15
-                       4.38v55.13H327.5c-27.81 0-36.51 17.26-36.51 35v42h62.12l-9.92 64.77H291v156.54c107.1-16.81 189-109.48 189-221.31"
-                            fill-rule="evenodd"
-                      />
-                </svg>
+                    <svg viewBox="0 0 320 512" fill="currentColor" class="w-5 h-5">
+                        <path d="M279.14 288l14.22-92.66h-88.91v-60.13c0-25.35 12.42-50.06 52.24-50.06h40.42V6.26S260.43 0 225.36 0c-73.22 0-121.08 44.38-121.08 124.72v70.62H22.89V288h81.39v224h100.17V288z"></path>
+                    </svg>
+                </div>
+            </li>
+            <li class="relative group">
+                <div class="w-12 h-12 bg-white text-lg text-gray-900 flex justify-center items-center rounded-full shadow-lg transition-transform transform group-hover:scale-110 cursor-pointer">
+        <span class="absolute top-[-40px] left-1/2 transform -translate-x-1/2 bg-cobalt-900 text-sm text-white px-2 py-1 rounded-md shadow-md opacity-0 group-hover:opacity-100 group-hover:translate-y-[-10px] transition duration-300">
+          X
         </span>
-
-        <span class="block py-4 w-32">
-                <svg xmlns="http://www.w3.org/2000/svg"
-                     width="30"
-                     height="30"
-                     class="ionicon" viewBox="0 0 512 512">
-
-                      <path d="M480 257.35c0-123.7-100.3-224-224-224s-224 100.3-224 224c0 111.8 81.9 204.47 189
-                      221.29V322.12h-56.89v-64.77H221V208c0-56.13 33.45-87.16 84.61-87.16 24.51 0 50.15 4.38 50.15
-                       4.38v55.13H327.5c-27.81 0-36.51 17.26-36.51 35v42h62.12l-9.92 64.77H291v156.54c107.1-16.81 189-109.48 189-221.31"
-                            fill-rule="evenodd"
-                      />
-                </svg>
+                    <svg viewBox="0 0 36 36" fill="currentColor" class="w-5 h-5">
+                        <path d="M20.79 18L34.71 2.1c.45-.6.54-1.44.21-2.1-.33-.72-1.02-1.14-1.77-1.14H26.13c-.81 0-1.57.33-2.1.9L17.94 9.33 12.12.9c-.54-.6-1.29-.9-2.1-.9H2.85C2.1 0 1.41.42 1.08 1.14c-.33.66-.24 1.5.21 2.1L15.21 18 1.29 33.9c-.45.6-.54 1.44-.21 2.1.33.72 1.02 1.14 1.77 1.14h7.17c.81 0 1.57-.33 2.1-.9l5.82-8.43 5.82 8.43c.54.6 1.29.9 2.1.9h7.17c.75 0 1.44-.42 1.77-1.14.33-.66.24-1.5-.21-2.1L20.79 18z"></path>
+                    </svg>
+                </div>
+            </li>
+            <li class="relative group">
+                <div class="w-12 h-12 bg-white text-lg text-gray-900 flex justify-center items-center rounded-full shadow-lg transition-transform transform group-hover:scale-110 cursor-pointer">
+        <span class="absolute top-[-40px] left-1/2 transform -translate-x-1/2 bg-cobalt-900 text-sm text-white px-2 py-1 rounded-md shadow-md opacity-0 group-hover:opacity-100 group-hover:translate-y-[-10px] transition duration-300">
+          Instagram
         </span>
-
-        <span class="block py-4 w-32">
-                <svg xmlns="http://www.w3.org/2000/svg"
-                     width="30"
-                     height="30"
-                     class="ionicon" viewBox="0 0 512 512">
-
-                      <path d="M480 257.35c0-123.7-100.3-224-224-224s-224 100.3-224 224c0 111.8 81.9 204.47 189
-                      221.29V322.12h-56.89v-64.77H221V208c0-56.13 33.45-87.16 84.61-87.16 24.51 0 50.15 4.38 50.15
-                       4.38v55.13H327.5c-27.81 0-36.51 17.26-36.51 35v42h62.12l-9.92 64.77H291v156.54c107.1-16.81 189-109.48 189-221.31"
-                            fill-rule="evenodd"
-                      />
-                </svg>
+                    <svg viewBox="0 0 16 16" fill="currentColor" class="w-5 h-5">
+                        <path d="M8 0C5.829 0 5.556.01 4.703.048 3.85.088 3.269.222 2.76.42a3.917 3.917 0 0 0-1.417.923A3.927 3.927 0 0 0 .42 2.76C.222 3.268.087 3.85.048 4.7.01 5.555 0 5.827 0 8.001c0 2.172.01 2.444.048 3.297.04.852.174 1.433.372 1.942.205.526.478.972.923 1.417.444.445.89.719 1.416.923.51.198 1.09.333 1.942.372C5.555 15.99 5.827 16 8 16s2.444-.01 3.298-.048c.851-.04 1.434-.174 1.943-.372a3.916 3.916 0 0 0 1.416-.923c.445-.445.718-.891.923-1.417.197-.509.332-1.09.372-1.942C15.99 10.445 16 10.173 16 8s-.01-2.445-.048-3.299c-.04-.851-.175-1.433-.372-1.941a3.926 3.926 0 0 0-.923-1.417A3.911 3.911 0 0 0 13.24.42c-.51-.198-1.092-.333-1.943-.372C10.443.01 10.172 0 7.998 0h.003zm-.717 1.442h.718c2.136 0 2.389.007 3.232.046.78.035 1.204.166 1.486.275.373.145.64.319.92.599.28.28.453.546.598.92.11.281.24.705.275 1.485.039.843.047 1.096.047 3.231s-.008 2.389-.047 3.232c-.035.78-.166 1.203-.275 1.485a2.47 2.47 0 0 1-.599.919c-.28.28-.546.453-.92.598-.28.11-.704.24-1.485.276-.843.038-1.096.047-3.232.047s-2.39-.009-3.233-.047c-.78-.036-1.203-.166-1.485-.276a2.478 2.478 0 0 1-.92-.598 2.48 2.48 0 0 1-.6-.92c-.109-.281-.24-.705-.275-1.485-.038-.843-.046-1.096-.046-3.233 0-2.136.008-2.388.046-3.231.036-.78.166-1.204.276-1.486.145-.373.319-.64.599-.92.28-.28.546-.453.92-.598.282-.11.705-.24 1.485-.276.738-.034 1.024-.044 2.515-.045v.002zm4.988 1.328a.96.96 0 1 0 0 1.92.96.96 0 0 0 0-1.92zm-4.27 1.122a4.109 4.109 0 1 0 0 8.217 4.109 4.109 0 0 0 0-8.217zm0 1.441a2.667 2.667 0 1 1 0 5.334 2.667 2.667 0 0 1 0-5.334z"></path>
+                    </svg>
+                </div>
+            </li>
+            <li class="relative group">
+                <div class="w-12 h-12 bg-white text-lg text-gray-900 flex justify-center items-center rounded-full shadow-lg transition-transform transform group-hover:scale-110 cursor-pointer">
+        <span class="absolute top-[-40px] left-1/2 transform -translate-x-1/2 bg-cobalt-900 text-sm text-white px-2 py-1 rounded-md shadow-md opacity-0 group-hover:opacity-100 group-hover:translate-y-[-10px] transition duration-300">
+          YouTube
         </span>
+                    <svg viewBox="0 0 576 512" fill="currentColor" class="w-5 h-5">
+                        <path d="M549.655 124.083c-6.281-23.689-24.852-42.273-48.478-48.654C456.566 64 288 64 288 64s-168.566 0-213.177 11.429c-23.626 6.381-42.197 24.965-48.478 48.654C16 168.694 16 256 16 256s0 87.306 10.345 131.917c6.281 23.689 24.852 42.273 48.478 48.654C119.434 448 288 448 288 448s168.566 0 213.177-11.429c23.626-6.381 42.197-24.965 48.478-48.654C560 343.306 560 256 560 256s0-87.306-10.345-131.917zM232 336V176l142 80-142 80z"></path>
+                    </svg>
+                </div>
+            </li>
+        </ul>
+        <ul class="flex  justify-center items-center h-32  order-1">
+            <li>
+                <a href="#">
+                    <span>Terms & Conditions</span>
+                </a>
+            </li>
+            <li>
+                <a href="#">
+                    <span>Terms & Conditions</span>
+                </a>
+            </li>
+            <li>
+                <a href="#">
+                    <span>Terms & Conditions</span>
+                </a>
+            </li>
+        </ul>
     </div>
 </footer>
